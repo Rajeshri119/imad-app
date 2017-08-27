044864bd73dd41d6ccfcb1b5159d43df0a6f7056
Rajeshri119 <30728146+Rajeshri119@users.noreply.github.com>
2017-08-27 23:54:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var crypto=require('crypto');
+var bodyParser=require('body-parser');
 var config={
     user:'rajeshri119',
     database:'rajeshri119',
@@ -13,7 +14,7 @@ var config={
 };
 var app = express();
 app.use(morgan('combined'));
-
+app.user(bodyParser.json());
 
 
 var articles={
@@ -91,6 +92,23 @@ function hash(input,salt)
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return ["pbkdf2Sync","10000",salt,hashed.toString('hex')].join('$');
 }
+app.post('/create-user',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt=crypto.getRamdomBytes(128).toString('hex');
+    var dbString=hash(password,salt);
+    pool.query('INSERT INTO "User"(Username,Password) VALUES ($1,$2)',[username,dbString],function(err,result){
+        
+             if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            res.send(JSON.stringify(result.rows));
+        }
+        });
+});
 app.get('/hash/:input',function(req,res){
     var hashedString=hash(req.params.input,'this-is-some-random-string');
     res.send(hashedString);
