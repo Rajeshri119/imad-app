01b6e2d476f0a9b327a25aa9e9d562bf122a276c
Rajeshri119 <30728146+Rajeshri119@users.noreply.github.com>
2017-08-31 22:11:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,3 +12,31 @@ img.onclick=function(){
     var interval=setInterval(moveRight,100);
     img.style.marginLeft='100px';
 };
+
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    
+    var request= new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+         if(request.readyState===XMLHttpRequest.DONE){
+             if(request.status===200){
+                 alert('Logged in successfully');
+             }
+             else if(request.status===403){
+                 alert('Username/Password is incorrect');
+             }
+             else if(request.status===500){
+                 alert('Soemthing went wrong on the server');
+             }
+             }
+         };
+         var username=document.getElementById('username').value;
+         var password=document.getElementById('password').value;
+         console.log(username);
+         console.log(password);
+         request.open('POST','http://rajeshri119.imad.hasura-app.io/login',true);
+         request.setRequestHeader('Content-Type','application/json');
+         request.send(JSON.stringify({username:username,password:password}));
+    };
+   
